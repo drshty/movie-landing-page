f03f2bc8abb1d93324fc4f1d1f9d8ea3fa12acf5
giselle <brownthirst00@gmail.com>
2023-02-28 15:42:24 -0400

    made it fully responsive and added scroll behavior

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
+menu.addEventListener("click", () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+});
+
+document.querySelector("#scrolling").addEventListener("click", () => {
+  window.scroll({
+    top: 600,
+    behavior: "smooth",
+  });
 });
 
 const sr = ScrollReveal({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
+  distance: "65px",
+  duration: 2600,
+  delay: 450,
+  reset: true,
 });
 
-sr.reveal('.hero-text', { delay: 200, origin: 'top' });
-sr.reveal('.hero-img', { delay: 450, origin: 'top' });
-sr.reveal('.icons', { delay: 500, origin: 'left' });
-sr.reveal('.scroll-down', { delay: 500, origin: 'right' });
+sr.reveal(".hero-text", { delay: 200, origin: "top" });
+sr.reveal(".hero-img", { delay: 450, origin: "top" });
+sr.reveal(".icons", { delay: 500, origin: "left" });
+sr.reveal(".scroll-down", { delay: 500, origin: "right" });
